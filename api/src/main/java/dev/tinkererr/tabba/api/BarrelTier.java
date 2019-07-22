fbde10018e3efbfc4520df3bd1b1f888a19a2e02
Luke Carr <me@tinkererr.dev>
2019-07-22 15:59:34 +0100

    Added field to BarrelTier enum representing tier upgrade path

--- api/src/main/java/dev/tinkererr/tabba/api/BarrelTier.java
+++ api/src/main/java/dev/tinkererr/tabba/api/BarrelTier.java
@@ -8,36 +8,38 @@ import java.math.BigInteger;
 public enum BarrelTier {
 
     /**
-     * The wood tier is the lowest tier {@link Barrel} and can store 4096 of one item.
+     * The nether star tier is the last tier of {@link Barrel} and can store an unlimited amount of one item. The actual
+     * maximum capacity is limited to the maximum size of a {@link java.math.BigInteger}.
      */
-    WOOD(1, new BigInteger("4096")),
-    /**
-     * The iron tier is the second tier of {@link Barrel} and can store 16384 of one item (4x as many as
-     * {@link BarrelTier#WOOD}).
-     */
-    IRON(2, new BigInteger("16384")),
-    /**
-     * The gold tier is the third tier of {@link Barrel} and can store 131072 of one item (8x as many as
-     * {@link BarrelTier#IRON}).
-     */
-    GOLD(3, new BigInteger("131072")),
+    NETHER_STAR(5, null, null),
     /**
      * The diamond tier is the fourth tier of {@link Barrel} and can store 16777216 of one item (128x as many as
      * {@link BarrelTier#GOLD}).
      */
-    DIAMOND(4, new BigInteger("16777216")),
+    DIAMOND(4, new BigInteger("16777216"), NETHER_STAR),
     /**
-     * The nether star tier is the last tier of {@link Barrel} and can store an unlimited amount of one item. The actual
-     * maximum capacity is limited to the maximum size of a {@link java.math.BigInteger}.
+     * The gold tier is the third tier of {@link Barrel} and can store 131072 of one item (8x as many as
+     * {@link BarrelTier#IRON}).
      */
-    NETHER_STAR(5, null);
+    GOLD(3, new BigInteger("131072"), DIAMOND),
+    /**
+     * The iron tier is the second tier of {@link Barrel} and can store 16384 of one item (4x as many as
+     * {@link BarrelTier#WOOD}).
+     */
+    IRON(2, new BigInteger("16384"), GOLD),
+    /**
+     * The wood tier is the lowest tier {@link Barrel} and can store 4096 of one item.
+     */
+    WOOD(1, new BigInteger("4096"), IRON);
 
     private final int id;
     private final BigInteger capacity;
+    private final BarrelTier nextTier;
 
-    BarrelTier(int id, BigInteger capacity) {
+    BarrelTier(int id, BigInteger capacity, BarrelTier nextTier) {
         this.id = id;
         this.capacity = capacity;
+        this.nextTier = nextTier;
     }
 
     /**
@@ -56,7 +58,16 @@ public enum BarrelTier {
      * @return The capacity of this tier.
      */
     public BigInteger getCapacity() {
-        return capacity;
+        return this.capacity;
+    }
+
+    /**
+     * Returns the tier that this tier can be upgraded to. If this is null then this tier cannot be upgraded further.
+     *
+     * @return The tier this tier will upgrade to.
+     */
+    public BarrelTier getNextTier() {
+        return this.nextTier;
     }
 
     /**
